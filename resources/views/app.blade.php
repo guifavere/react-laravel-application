3e57ac7ab55d20572f64ac08e8212ca9078fb682
Guilherme de Favere <guilherme.favere@gmail.com>
2019-10-09 20:26:17 -0300

    Update the assets for secure loading

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,10 +7,10 @@
         <!-- csrf token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>React laravel application</title>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app"></div>
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ secure_asset('js/app.js') }}"></script>
     </body>
 </html>
